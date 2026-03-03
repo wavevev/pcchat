9025a4256559829a8c29484cd9fb2aceb273c37f
정혀니야 <mac@maegbug-ui-MacBookPro.local>
2026-03-03 18:46:50 +0900

    fix static folder path

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server);
 
-app.use(express.static(__dirname));
+app.use(express.static("public"));
 
 let users = {};
 let admins = new Set(["9996"]);
